f4198b6f438579c78c8950fcd4d66aaa39e93bc3
Jonathan <mcjhona069@gmail.com>
2022-06-14 20:35:03 -0500

    Creación de primeras vistas

--- idgs9A/videogames/views.py
+++ idgs9A/videogames/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    return render(request, 'core/home.html')
+
+def about(request):
+    return render(request, 'core/about.html')
+
+def gameDetail(request):
+    return render(request, 'core/gameDetail.html')

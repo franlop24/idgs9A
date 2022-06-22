047ec6bf78563bae0672584235e5514887607910
clarissa2307 <Marthaleo07@gmail.com>
2022-06-22 18:30:38 -0500

    Organizando mejor nuestras URLs

--- idgs9A/Musica/urls.py
+++ idgs9A/Musica/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('', views.home, name="home"),
     path('cancion/', views.cancion, name="cancion"),
     path('album/', views.album, name="album"),
     path('store/', views.store, name="store"),

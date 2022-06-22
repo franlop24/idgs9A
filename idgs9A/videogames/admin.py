756a006d7a0efe051f5353ba24686b6fc808ed23
Luis Angel Santos <santosangel748@gmail.com>
2022-06-22 09:13:48 -0500

    Optimizando proyecto

--- idgs9A/videogames/admin.py
+++ idgs9A/videogames/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Videogame
+from .models import Videogames
 # Register your models here.
 class VideogameAdmin(admin.ModelAdmin):
     readonly_fields = ('created', 'updated')
 
-admin.site.register(Videogame, VideogameAdmin)
+admin.site.register(Videogames, VideogameAdmin)
 # Register your models here.

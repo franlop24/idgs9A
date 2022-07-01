8b5d030e950693e06f94a0e14b840655e59b9040
Víctor Tamayo <angelicahuescagonzalez@gmail.com>
2022-07-01 13:56:34 -0500

    templates

--- idgs9A/books/views.py
+++ idgs9A/books/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request, 'books/home.html')
+
+def books(request):
+    return render(request, 'books/books.html')
+
+def prestamos(request):
+    return render(request, 'books/prestamos.html')

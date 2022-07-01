883a07405f2b407daa520ea7cb6f4647f310c54d
Víctor Tamayo <angelicahuescagonzalez@gmail.com>
2022-06-30 19:56:51 -0500

    Secret key

--- idgs9A/idgs9A/settings.py
+++ idgs9A/idgs9A/settings.py
@@ -9,9 +9,8 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
-from .config_local import secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = secret_key()
+SECRET_KEY = 'django-insecure-pzfvz-@(v%sklgrnl^b(w((qxp1vlym0%2c*ee7szhxl2*o5^o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

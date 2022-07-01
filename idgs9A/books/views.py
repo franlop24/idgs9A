84204c37c9070d590de7f2ddcb738f5918b1c23b
Luism2 <luisjuares73@gmail.com>
2022-07-01 15:15:33 -0600

    importacion de models a views

--- idgs9A/books/views.py
+++ idgs9A/books/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect
+from books.models import Books, Users
 
 # Create your views here.
 def home(request):
